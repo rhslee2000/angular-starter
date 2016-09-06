222234089f7575ce617fa0a9e8a0b96554863a8b
rhslee2000 <rhslee2000@yahoo.com.au>
2016-09-06 15:58:50 +1000

    replaced gulp-browserify with browserify fixed up watch/live reload

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,21 @@ var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
-var browserify = require('gulp-browserify');
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
 var concat = require('gulp-concat');
 
 // browsersync for live reload
 var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-var ngAnnotate = require('gulp-ng-annotate');
+/*
+ * need this to fix angular minification problem,
+ * see http://blog.jhades.org/what-every-angular-project-likely-needs-and-a-gulp-build-to-provide-it/
+ */ 
+var  ngAnnotate = require('browserify-ngannotate');
 
 // =======================================================================
 // File Paths
@@ -60,47 +68,52 @@ gulp.task('copy-html-files', function () {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('connect', function () {
+gulp.task('connect', ['browserify'], function () {
 
+/*
+  Note: need to add reloadDelay to prevent browser trying to reload when node is not ready and end up hanging browse
+  see https://github.com/BrowserSync/browser-sync/issues/392
+*/
     browserSync.init({
         open:  true ,
+        reloadDelay : 2000,
         server: {
             baseDir: 'app/'
         }
     });
 
     gulp.watch([
-      'app/**/*' , '!app/libs/**'
+      'app/**/*' , '!app/libs/**', '!app/bundled.js'
     ] , ['serve:reload'] );
+    
 });
 
 gulp.task('browserify', function() {
-  gulp.src(['app/app.js'])
-  .pipe(ngAnnotate())
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  .pipe(concat('bundled.js'))
-  .pipe(gulp.dest('./app'))
+  browserify('app/app.js', {
+        debug: true,
+        transform: [ngAnnotate]
+      })
+      .bundle()
+      .pipe(source('bundled.js'))
+      .pipe(gulp.dest('./app'));
 });
 
 gulp.task('browserifyDist', function() {
-  gulp.src(['app/app.js'])
-  .pipe(ngAnnotate())
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  .pipe(concat('bundled.js'))
-  .pipe(gulp.dest('./dist'))
+    browserify('app/app.js', {
+        debug: true,
+        transform: [ngAnnotate]
+      })
+      .bundle()
+      .pipe(source('bundled.js'))
+      .pipe(gulp.dest('./dist'));
 });
 
 // =======================================================================
 // Watch for changes
 // =======================================================================
-gulp.task('serve:reload' , []  , function(){
-  browserSync.reload();
+gulp.task('serve:reload' , ['browserify'], function(done){
+  reload();
+  done();
 });
 
 // default task
